12108799c40642cfd716f309576ae49457a8b8e3
YashhhMore <yashm200133@gmail.com>
2026-02-06 20:04:30 +0530

    Update message in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type": "application/json"});
   res.end(JSON.stringify({
-    message: "Hello from a real Dockerized Application🚀",
+    message: "Hello from a real Don🚀",
     container: true
   }));
 });
